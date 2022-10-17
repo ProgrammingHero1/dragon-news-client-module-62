f80a3da364daecf813fb5c2661a3c55a02c402b9
Programming Hero <programming.hero1@gmail.com>
2022-10-16 19:57:31 -0500

    initial project setup

--- src/__steps.js
+++ src/__steps.js
@@ -1,4 +1,5 @@
 /* 
+BASIC PROJECT SETUP
 1. create firebase app
 2. get firebase configuration inside firebase.config.js
 3. make sure to export app from firebase config
@@ -7,5 +8,10 @@
 5. install bootstrap and react-bootstrap
 6. make sure to import bootstrap css in the index.js file
 7. 
+8. 
+*/
 
+/* 
+ROUTER SETUP
+1. 
 */
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
-            
+            <Outlet></Outlet>
         </div>
     );
 };
