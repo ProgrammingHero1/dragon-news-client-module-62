dbecb230e846fa64965e7668272abe0a33b7c6e4
Programming Hero <programming.hero1@gmail.com>
2022-10-16 22:32:08 -0500

    set state for categories

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
     return (
         <div>
-            <h2>Left Side Nav</h2>
+            <h4>All Category</h4>
         </div>
     );
 };
