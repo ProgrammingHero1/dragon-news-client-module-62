b4837dcfb7171c4da950e7d7d822a3a0d82df016
Programming Hero <programming.hero1@gmail.com>
2022-10-17 22:29:31 -0500

    style news summary card

--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image'
+import { FaRegBookmark, FaShareAlt } from 'react-icons/fa';
 
-const NewsSummaryCard = ({news}) => {
+const NewsSummaryCard = ({ news }) => {
+    const { _id, title, author, details, image_url, total_view } = news;
+    console.log(news);
     return (
-        <div>
-            {news.title}
-        </div>
+        <Card className="mb-5">
+            <Card.Header className='d-flex justify-content-between align-items-center'>
+                <div className='d-flex'>
+                    <Image
+                        roundedCircle
+                        className='me-2'
+                        src={author.img}
+                        style={{height: '60px'}}
+                    ></Image>
+                    <div>
+                        <p>{author.name}</p>
+                        <p>{author.published_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <FaRegBookmark></FaRegBookmark>
+                    <FaShareAlt></FaShareAlt>
+                </div>
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Text>
+                    {
+                        details.length > 250 ?
+                            <p>{details.slice(0, 250) + '...'} <Link to={`/news/${_id}`}>Read More</Link> </p>
+                            :
+                            <p>{details}</p>
+                    }
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer className="text-muted">2 days ago</Card.Footer>
+        </Card>
     );
 };
 
