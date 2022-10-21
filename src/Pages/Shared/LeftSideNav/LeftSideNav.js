1cd01f7ed3ca39da556227c33514807308080853
Programming Hero <programming.hero1@gmail.com>
2022-10-20 20:37:20 -0500

    updated localhost links by live site link

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -5,7 +5,7 @@ const LeftSideNav = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect( () =>{
-        fetch('http://localhost:5000/news-categories')
+        fetch('https://dragon-news-server-seven.vercel.app/news-categories')
         .then( res => res.json())
         .then(data => setCategories(data));
     }, [])
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -17,17 +17,17 @@ export const routes = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
-                loader: () => fetch('http://localhost:5000/news')
+                loader: () => fetch('https://dragon-news-server-seven.vercel.app/news')
             },
             {
                 path: '/category/:id',
                 element: <Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/category/${params.id}`)
+                loader: ({params}) => fetch(`https://dragon-news-server-seven.vercel.app/category/${params.id}`)
             },
             {
                 path: '/news/:id',
                 element: <PrivateRoute><News></News></PrivateRoute>,
-                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({params}) => fetch(`https://dragon-news-server-seven.vercel.app/news/${params.id}`)
             },
             {
                 path: '/login',
